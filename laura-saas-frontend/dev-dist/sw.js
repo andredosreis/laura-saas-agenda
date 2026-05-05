43404bfadb7e9bd0fff3cff3422ea73c6b92f87c
Andre dos Reis <andredosreis@gmail.com>
2026-05-05 20:44:56 +0100

    fix(sw): update revision for index.html in service worker

--- laura-saas-frontend/dev-dist/sw.js
+++ laura-saas-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.in6uhnq420g"
+    "revision": "0.d1utbga4gkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
